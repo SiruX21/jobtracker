8f6b3ac42c97d7cc8f031365ba2e8295b3b06671
Syrus Tolentino <syrustolentino@outlook.com>
2025-07-30 22:08:37 -0500

    more ui update to jobcards

--- front-end/src/components/tracker/JobCards.jsx
+++ front-end/src/components/tracker/JobCards.jsx
@@ -51,10 +51,10 @@ function JobCards({
                   e.stopPropagation();
                   deleteJob(jobs.findIndex(j => j.id === job.id));
                 }}
-                className="absolute top-2 right-2 p-1.5 bg-red-100 dark:bg-red-900/50 text-red-600 dark:text-red-400 rounded-full hover:bg-red-200 dark:hover:bg-red-900 transition-colors opacity-80 hover:opacity-100 z-10"
+                className="absolute top-2 right-2 p-1.5 text-gray-400 dark:text-gray-500 rounded-full hover:text-gray-600 dark:hover:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 transition-colors opacity-60 hover:opacity-100"
                 title="Delete application"
               >
-                <FaTrash className="w-2 h-2" />
+                <FaTrash className="w-3 h-3" />
               </button>
               
               <div className="flex items-center space-x-3">
