9bb39c6464b47f72fc91fce120df2b41d57307ca
Syrus Tolentino <syrustolentino@outlook.com>
2025-08-08 16:39:50 -0500

    fix admin panel.jsx

--- front-end/src/AdminPanel.jsx
+++ front-end/src/AdminPanel.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Transition } from '@headlessui/react';
 import axios from 'axios';
 import Cookies from 'js-cookie';
 import { showToast } from './utils/toast';
