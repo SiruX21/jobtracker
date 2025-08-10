b7f9fcb7bb82f7156664ba450f201e9c1a245128
Syrus Tolentino <syrustolentino@outlook.com>
2025-08-09 20:51:31 -0500

    toastify fix??

--- front-end/src/utils/toast.js
+++ front-end/src/utils/toast.js
@@ -1,21 +1,6 @@
 import { toast } from 'react-toastify';
 
-// Simple, working toast implementation
-export const showToast = {
-  success: (message, options = {}) => {
-    return toast.success(message, options);
-  },
-  error: (message, options = {}) => {
-    return toast.error(message, options);
-  },
-  info: (message, options = {}) => {
-    return toast.info(message, options);
-  },
-  warning: (message, options = {}) => {
-    return toast.warning(message, options);
-  }
-};
+export const showToast = toast;
+export const showCriticalToast = toast;
 
-export const showCriticalToast = showToast;
-
-export default showToast;
+export default toast;
